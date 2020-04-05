046d61ec497c1fd14a4f5e96b178fe474d8026aa
andrey-pep <andrey-pep@mail.ru>
2020-04-05 20:35:30 +0300

    Create task5.py

---
+++ maths7/task5.py
@@ -0,0 +1,27 @@
+import numpy as np
+from pylab import *
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def Q(x, y, z):
+    return (x**2 + y**2 + z**2)
+
+fig = figure()
+ax = Axes3D(fig)
+
+x = np.linspace(-20,20,201)
+y = np.linspace(-20,20,201)
+X, Y = np.meshgrid(x, y)
+Z1 = Q(X, Y, (x + 2 * y - 1))
+Z2 = Q(X, Y, (-4 * x + 2.5 * y + 6))
+
+surf = ax.plot_surface(X, Y, Z1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+plt.grid(True)
+plt.show()
+
+#получили поверхность, по которой можно определить точку минимума
+A = np.array([[1,2,-1], [8,-5,2]])
+B = np.array([1,12])
+np.linalg.lstsq(A,B, rcond=None)
